3af4a4d26cf6839c1e8312833b20a1b9a26e9bdd
Skulliks <skullxyoutb@gmail.com>
2026-02-14 18:17:39 +0100

    changed styles and structure of pages

--- src/pages/Diary.tsx
+++ src/pages/Diary.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDictContext } from "../context/DictContext";
 import { useTitleContext } from "../context/TitleContext";
 import { Box } from "@mui/material";
+import Affiche from "../assets/affiche.jpg";
 
 interface EventProps {
   month: string;
@@ -20,17 +21,31 @@ export const Diary = () => {
   if (!dict) return;
 
   return (
-    <Box id="contentDiary">
-      {dict.events.map((event: EventProps) => {
-        return (
-          <Box key={event.title} className="diary-one-box">
-            <span className="diary-month">{event.month}</span>
-            <span className="diary-day">{event.day}</span>
-            <span className="diary-title">{event.title}</span>
-          </Box>
-        );
-      })}
-    </Box>
+    <>
+      {dict.next_exposition_title && (
+        <Box
+          sx={{
+            mt: 5,
+          }}
+          id="next-exposition-box"
+        >
+          <figure>
+            <img src={Affiche} alt="Affiche" />
+          </figure>
+        </Box>
+      )}
+      {/*  <Box id="contentDiary">
+       {dict.events.map((event: EventProps) => {
+         return (
+           <Box key={event.title} className="diary-one-box">
+             <span className="diary-month">{event.month}</span>
+             <span className="diary-day">{event.day}</span>
+             <span className="diary-title">{event.title}</span>
+           </Box>
+         );
+       })}
+     </Box> */}
+    </>
   );
 };
 
--- src/pages/Expositions.tsx
+++ src/pages/Expositions.tsx
@@ -1,48 +1,69 @@
 import { useTitleContext } from "../context/TitleContext";
 import { useDictContext } from "../context/DictContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import type { ExpositionProps } from "../types";
 import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
+import Affiche from "../assets/affiche.jpg";
 
 export const Expositions = () => {
   const { setTitle } = useTitleContext();
   const { dict } = useDictContext();
-  console.log(dict.expositions.reverse());
+  const [expositions, setExpositions] = useState<ExpositionProps[]>();
 
   useEffect(() => {
     if (dict) setTitle(dict.exposition_title);
   }, [dict, setTitle]);
 
-  if (!dict) return;
+  useEffect(() => {
+    if (dict)
+      setExpositions(
+        dict.expositions.sort(
+          (a: ExpositionProps, b: ExpositionProps) => b.id - a.id,
+        ),
+      );
+  }, [dict]);
+
+  if (!dict || !expositions) return;
 
   return (
-    <Box id="contentExpositions">
-      {dict.expositions.map((exposition: ExpositionProps) => {
-        return (
-          <Link
-            to={{ pathname: `/exposition/${exposition.id}` }}
-            key={exposition.title}
-            style={{
-              display: "block",
-            }}
-          >
-            <figure>
-              <img
-                src={`${import.meta.env.BASE_URL}exp_previews/${
-                  exposition.previewName
-                }`}
-                alt=""
-              />
-              <figcaption>
-                <strong className="strong">{exposition.date}</strong>
-                {exposition.title}
-              </figcaption>
-            </figure>
-          </Link>
-        );
-      })}
-    </Box>
+    <>
+      {dict.next_exposition_title && (
+        <Box id="next-exposition-box">
+          <h1>{dict.next_exposition_title}</h1>
+          <figure>
+            <img src={Affiche} alt="Affiche" />
+          </figure>
+        </Box>
+      )}
+      <h1 className="title_our_exp">{dict.exp_title}</h1>
+      <Box id="contentExpositions">
+        {expositions.map((exposition) => {
+          return (
+            <Link
+              to={{ pathname: `/exposition/${exposition.id}` }}
+              key={exposition.title}
+              style={{
+                display: "block",
+              }}
+            >
+              <figure>
+                <img
+                  src={`${import.meta.env.BASE_URL}exp_previews/${
+                    exposition.previewName
+                  }`}
+                  alt=""
+                />
+                <figcaption>
+                  <strong className="strong">{exposition.date}</strong>
+                  {exposition.title}
+                </figcaption>
+              </figure>
+            </Link>
+          );
+        })}
+      </Box>
+    </>
   );
 };
 
